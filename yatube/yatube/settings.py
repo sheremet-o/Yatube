07f3f8bfeb75fc30ae284084a06d1928dcebd929
Oksana Sheremet <96820594+waspywasp@users.noreply.github.com>
2022-05-05 12:10:21 +0300

    Update settings.py

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -1,5 +1,8 @@
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
@@ -8,10 +11,10 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '89wxey-$twab%u_q^x)^tv-bjgx+d42!2j9ht9llzrznjn3r3%'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '*',
